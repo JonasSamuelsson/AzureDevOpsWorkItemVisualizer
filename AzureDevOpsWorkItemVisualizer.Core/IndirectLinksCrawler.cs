44131164b728cbfd1ad9d0cf2fb868c30a682204
Jonas Samuelsson <jonas.samuelsson@cab.se>
2021-09-17 10:35:39 +0200

    made it possible to select which crawler to use

--- AzureDevOpsWorkItemVisualizer.Core/IndirectLinksCrawler.cs
+++ AzureDevOpsWorkItemVisualizer.Core/IndirectLinksCrawler.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using AzureDevOpsWorkItemVisualizer.Core.Model;
+using Handyman.Extensions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AzureDevOpsWorkItemVisualizer.Core.Model;
-using Handyman.Extensions;
 
 namespace AzureDevOpsWorkItemVisualizer.Core
 {
@@ -18,127 +17,27 @@ namespace AzureDevOpsWorkItemVisualizer.Core
 
       public async Task<WorkItemCollection> GetData(ISet<int> workItemIds, ISet<WorkItemType> workItemTypes, bool includeFinishedWorkItems)
       {
-         var data = await LoadData(workItemIds, workItemTypes, includeFinishedWorkItems);
-
-         return BuildGraph(workItemIds, data);
-      }
-
-      private async Task<WorkItemCollection> LoadData(ISet<int> workItemIds, ISet<WorkItemType> workItemTypes, bool includeFinished)
-      {
-         var data = new WorkItemCollection();
+         var result = await _client.GetData(workItemIds, workItemTypes, includeFinishedWorkItems);
 
          while (true)
          {
-            // get all work items from origin to top
-            var response = await _client.GetData(workItemIds, workItemTypes, includeFinished);
+            var ids = result.WorkItems.Select(x => x.Id).ToList();
 
-            response.Links.ForEach(x => data.Links.Add(x));
-            response.WorkItems.ForEach(x => data.WorkItems.Add(x));
-
-            var loadedWorkItemIds = data.WorkItems.Select(x => x.Id).ToSet();
-
-            workItemIds = data.Links
-               .Select(x => x.FromWorkItemId)
-               .Where(x => loadedWorkItemIds.Contains(x) == false)
-               .ToSet();
-
-            if (workItemIds.Any() == false)
-               break;
-         }
-
-         while (true)
-         {
-            // get all work items from top to bottom
-            var loadedWorkItemIds = data.WorkItems
-               .Select(x => x.Id)
-               .ToSet();
-
-            workItemIds = data.Links
-               .Select(x => x.ToWorkItemId)
-               .Where(x => loadedWorkItemIds.Contains(x) == false)
+            workItemIds = result.Links
+               .SelectMany(x => new[] { x.FromWorkItemId, x.ToWorkItemId })
+               .Where(x => ids.Contains(x) == false)
                .ToSet();
 
             if (workItemIds.Any() == false)
                break;
 
-            var response = await _client.GetData(workItemIds, workItemTypes, includeFinished);
+            var response = await _client.GetData(workItemIds, workItemTypes, includeFinishedWorkItems);
 
-            response.Links.ForEach(x => data.Links.Add(x));
-            response.WorkItems.ForEach(x => data.WorkItems.Add(x));
-         }
-
-         return data;
-      }
-
-      private static WorkItemCollection BuildGraph(ISet<int> workItemIds, WorkItemCollection data)
-      {
-         var fromWorkItemIds = GetEdgeWorkItemIds(data.Links, workItemIds, x => x.ToWorkItemId, x => x.FromWorkItemId);
-         var toWorkItemIds = GetEdgeWorkItemIds(data.Links, workItemIds, x => x.FromWorkItemId, x => x.ToWorkItemId);
-
-         workItemIds = GetWorkItemIds(data.Links, fromWorkItemIds, toWorkItemIds);
-
-         return new WorkItemCollection
-         {
-            Links = data.Links
-               .Where(x => workItemIds.Contains(x.FromWorkItemId) && workItemIds.Contains(x.ToWorkItemId))
-               .ToSet(),
-            WorkItems = data.WorkItems
-               .Where(x => workItemIds.Contains(x.Id))
-               .ToSet()
-         };
-      }
-
-      private static ISet<int> GetEdgeWorkItemIds(ISet<Link> links, ISet<int> workItemIds, Func<Link, int> sourceIdSelector, Func<Link, int> targetIdSelector)
-      {
-         var result = new HashSet<int>();
-
-         while (workItemIds.Any())
-         {
-            workItemIds
-               .Where(x => links.Any(y => x == sourceIdSelector(y)) == false)
-               .ForEach(x => result.Add(x));
-
-            workItemIds = links
-               .Where(x => workItemIds.Contains(sourceIdSelector(x)))
-               .Select(targetIdSelector)
-               .ToSet();
+            response.Links.ForEach(x => result.Links.Add(x));
+            response.WorkItems.ForEach(x => result.WorkItems.Add(x));
          }
 
          return result;
-      }
-
-      private static ISet<int> GetWorkItemIds(ISet<Link> links, ISet<int> fromWorkItemIds, ISet<int> toWorkItemIds)
-      {
-         var workItemIds = new HashSet<int>();
-
-         foreach (var fromWorkItemId in fromWorkItemIds)
-         {
-            foreach (var path in GetPaths(links, fromWorkItemId, toWorkItemIds))
-            {
-               path.ForEach(x => workItemIds.Add(x));
-            }
-         }
-
-         return workItemIds;
-      }
-
-      private static IEnumerable<IEnumerable<int>> GetPaths(ISet<Link> links, int fromWorkItemId, ISet<int> toWorkItemIds)
-      {
-         if (toWorkItemIds.Contains(fromWorkItemId))
-         {
-            yield return new[] { fromWorkItemId };
-            yield break;
-         }
-
-         foreach (var link in links.Where(x => x.FromWorkItemId == fromWorkItemId))
-         {
-            foreach (var path in GetPaths(links, link.ToWorkItemId, toWorkItemIds))
-            {
-               var list = new List<int>(new[] { fromWorkItemId });
-               list.AddRange(path);
-               yield return list;
-            }
-         }
       }
    }
 }
