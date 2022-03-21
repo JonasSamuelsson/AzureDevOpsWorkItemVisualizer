2440f333b6e19527cf407dbc85f18b0aac7cfceb
Jonas Samuelsson <jonassamuelsson@outlook.com>
2022-03-21 09:05:39 +0100

    console settings file

--- AzureDevOpsWorkItemVisualizer.Console/Program.cs
+++ AzureDevOpsWorkItemVisualizer.Console/Program.cs
@@ -1,6 +1,11 @@
 ﻿using AzureDevOpsWorkItemVisualizer.Core;
 using AzureDevOpsWorkItemVisualizer.Core.Model;
+using Handyman.Extensions;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AzureDevOpsWorkItemVisualizer.Console
@@ -9,13 +14,21 @@ namespace AzureDevOpsWorkItemVisualizer.Console
    {
       public static async Task Main()
       {
-         var options = new AzureDevOpsClientOptions
-         {
-         };
+         var options = new AzureDevOpsClientOptions();
+         var workItemIds = new HashSet<int>();
 
-         var workItemIds = new HashSet<int>
+#if DEBUG
+         if (TryReadSettings(out var settings))
          {
-         };
+            options.Organization = settings.GetValueOrDefault("Organization", () => string.Empty);
+            options.PersonalAccessToken = settings.GetValueOrDefault("PersonalAccessToken", () => string.Empty);
+            options.Project = settings.GetValueOrDefault("Project", () => string.Empty);
+            workItemIds = settings.GetValueOrDefault("WorkItemIds", () => string.Empty)
+               .Split(new[] { ' ', ',', ';' }, StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToHashSet();
+         }
+#endif
 
          var includeWorkItemTypes = new HashSet<WorkItemType>
          {
@@ -33,6 +46,28 @@ namespace AzureDevOpsWorkItemVisualizer.Console
          // http://magjac.com/graphviz-visual-editor/ can be used to test the graph
 
          System.Console.WriteLine(graph);
+      }
+
+      private static bool TryReadSettings(out Dictionary<string, string> settings)
+      {
+         var file = "AzureDevOpsWorkItemsVisualizer.Console.settings.json";
+         var dir = Environment.CurrentDirectory;
+
+         while (dir != null)
+         {
+            var path = Path.Combine(dir, file);
+
+            if (File.Exists(path))
+            {
+               settings = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(path));
+               return true;
+            }
+
+            dir = Path.GetDirectoryName(dir);
+         }
+
+         settings = null;
+         return false;
       }
    }
 }
